a5934875bdd431588fcd1506f924e48409729402
hiropo20 <hmatsumoto20@gmail.com>
2022-02-18 10:54:46 +0900

    update

--- docker-comopse/config/njs-conf.d/njs-http_info.js
+++ docker-comopse/config/njs-conf.d/njs-http_info.js
@@ -39,14 +39,40 @@ function hello(r) {
 
 // reqall
 function reqall(r) {
-  let str = {};
-  str.headers = r.rawHeadersIn;
-  str.status = r.status;
-  str.httpversion = r.httpVersion;
-  str.method = r.method;
-  str.uri = r.uri;
-  str.args = r.variables.args;
-  str.reqbody = r.requestText;
+  let str = {
+    request: {},
+    network: {},
+    ssl: {},
+    session: {},
+    environment: {}
+  }
+  str.request.headers = r.rawHeadersIn;
+  str.request.status = r.status;
+  str.request.httpversion = r.httpVersion;
+  str.request.method = r.method;
+  str.request.scheme = r.variables.scheme;
+  str.request.uri = r.uri;
+  str.request.args = r.variables.args;
+  str.request.requestText = r.requestText;
+  str.request.fullPath = r.variables.request_uri;
+  str.request.body = r.requestBody != 'undefined' ? r.requestBody : undefined;
+
+  str.environment.hostname =  r.variables.hostname;
+
+  str.network.clientPort = r.variables.remote_port;
+  str.network.clientAddress = r.variables.remote_addr;
+  str.network.serverAddress = r.variables.server_addr;
+  str.network.serverPort = r.variables.server_port;
+
+  str.ssl.isHttps = r.variables.Https != 'undefined' && r.variables.Https == 'on' ? true : false;
+  str.ssl.sslProtocol = r.variables.sslProtocol != 'undefined' ? r.variables.ssl_protocol : undefined;
+  str.ssl.sslCipher = r.variables.sslCipher != 'undefined' ? r.variables.ssl_cipher : undefined;
+
+  str.session.httpConnection = r.variables.http_connection;
+  str.session.requestId = r.variables.request_id;
+  str.session.connection = r.variables.connection;
+  str.session.connectionNumber = r.variables.connection_requests;
+
   r.return(200, JSON.stringify(str));
   return;
 }
